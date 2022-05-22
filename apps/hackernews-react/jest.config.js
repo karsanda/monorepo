c90c3f0123de5510709f8d7f32276ec97a790f05
Pinto Karsanda <pintokarsanda@gmail.com>
2022-05-22 13:53:40 +0700

    rename workspace

--- apps/hackernews-react/jest.config.js
+++ apps/hackernews-react/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'hn-react',
+  displayName: 'hackernews-react',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/hn-react',
+  coverageDirectory: '../../coverage/apps/hackernews-react',
 };
--- apps/hackernews-react/src/app/app.tsx
+++ apps/hackernews-react/src/app/app.tsx
@@ -1,17 +1,17 @@
-import styled from '@emotion/styled'
-import useFetch from '../hooks/useFetch'
-import Item from './item'
+import styled from '@emotion/styled';
+import useFetch from '../hooks/useFetch';
+import Item from './item';
 
 const Header = styled.header`
   padding: 5px 10px;
   background-color: var(--dark-bg);
   color: var(--primary-color);
-`
+`;
 
 const Main = styled.main`
   padding: 10px 15px 15px 0;
   min-height: calc(100vh - 116px);
-`
+`;
 
 const Footer = styled.main`
   border-top: 1px solid #cccccc;
@@ -19,27 +19,34 @@ const Footer = styled.main`
   padding: 10px 0;
   text-align: center;
   font-size: 0.8em;
-`
+`;
 
 const List = styled.ol`
   margin: 0;
-`
+`;
 
 function App() {
-  const { data } = useFetch<string[]>('topstories')
+  const { data } = useFetch<string[]>('topstories');
   return (
     <>
       <Header>
         <h1>HN - React</h1>
       </Header>
       <Main>
-        <List>{data?.splice(0, 30).map(id => <Item key={id} id={id} />)}</List>
+        <List>
+          {data?.splice(0, 30).map((id) => (
+            <Item key={id} id={id} />
+          ))}
+        </List>
       </Main>
       <Footer>
-        ©{new Date().getFullYear()} Karsanda | <a href='https://github.com/karsanda/monorepo/tree/main/apps/hn-react'>HN - React</a>
+        ©{new Date().getFullYear()} Karsanda |{' '}
+        <a href="https://github.com/karsanda/monorepo/tree/main/apps/hn-react">
+          HN - React
+        </a>
       </Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
