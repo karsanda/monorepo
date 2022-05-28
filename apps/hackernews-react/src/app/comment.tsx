714d2dfbe5f914f2092fc074a89faddabac810de
Pinto Karsanda <pintokarsanda@gmail.com>
2022-05-28 15:17:00 +0700

    Add collapsible button for comments

--- apps/hackernews-react/src/app/comment.tsx
+++ apps/hackernews-react/src/app/comment.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import { formatDistance } from 'date-fns'
+import { useState } from 'react'
 import useFetch from '../hooks/useFetch'
 
 const Container = styled.article`
@@ -10,13 +11,29 @@ const Container = styled.article`
   }
 `
 
-const Subtitle = styled.p`
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const CollapsibleButton = styled.button`
+  font-size: 12px;
+  background: none;
+  outline: none;
+  border: none;
+  margin: 0 5px 0 0;
+  padding: 0;
+  cursor: pointer;
+`
+
+const Article = styled.p`
   margin: 5px 0;
   color: var(--gray);
   font-size: 11px;
 `
 
 const Content = styled.div`
+  margin-left: 17px;
   font-size: 12px;
 
   & p {
@@ -41,20 +58,24 @@ interface CommentData {
 
 function Comment({ id }: { id: number }) {
   const { data } = useFetch<CommentData>(`item/${id}`)
+  const [ isCollapse, setIsCollapse ] = useState(false)
 
   if (!data) return null
+  const { by, time, kids, text } = data
 
-  const { by, time, kids } = data
   return (
     <Container>
-      <Subtitle>
-        {by} {time && formatDistance(time * 1000, new Date(), { addSuffix: true })}
-      </Subtitle>
-      <Content dangerouslySetInnerHTML={{ __html: data.text }} />
-      {kids && (
-        <Child>
-          {kids.map(kid => <Comment id={kid} key={kid} /> )}
-        </Child>
+      <Header>
+        <CollapsibleButton onClick={() => setIsCollapse(!isCollapse)}>
+          {isCollapse ? '▼' : '▲'}
+        </CollapsibleButton>
+        <Article>
+          {by} {time && formatDistance(time * 1000, new Date(), { addSuffix: true })}
+        </Article>
+      </Header>
+      <Content dangerouslySetInnerHTML={{ __html: text }} />
+      {(kids && !isCollapse) && (
+        <Child>{kids.map(kid => <Comment id={kid} key={kid} /> )}</Child>
       )}
     </Container>
   )
